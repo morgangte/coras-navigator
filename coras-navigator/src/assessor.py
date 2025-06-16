1b07353e5454a3e755de6276d801083e49e941df
Morgan Gillette <morgan.gillette@sintef.no>
2025-06-16 09:30:04 +0200

    One or multiple threat scenarios

--- coras-navigator/src/assessor.py
+++ coras-navigator/src/assessor.py
@@ -13,7 +13,7 @@ class SimpleRiskAssessor(RiskAssessor):
     system_prompt = """
         You are an expert in cybersecurity risk assessment. From the description of a system and some context, you perform a cybersecurity risk assessment of the system.
         First, generate a high-level risk table that contains for each risk: Who/What causes the risk? How? What is the incident? What does it harm (asset)? What makes it possible (vulnerabilities)? During your analysis, examine different attack surfaces and various possible threats (human accidental, human deliberate, non-human threat, etc.). Think of at least 2 risks.
-        Then, from this high-level analysis, specify for each risk the following items: the threat, the threat scenario, the unwanted incident, the impacted assets, and the associated vulnerabilities.
+        Then, from this high-level analysis, specify for each risk the following items: the threat, one or multiple consecutive threat scenarios, the unwanted incident, the impacted assets, and the associated vulnerabilities.
         """
 
     def assess(self, description: str, context: str) -> str:
